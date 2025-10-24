349c2d6153f75ed260ee0463c2cdfe48ce3f5190
munnabiswas99 <biswas15-5261@diu.edu.bd>
2025-10-24 22:17:58 +0600

    Register form Created

---
+++ src/assets/components/login/Login.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Login = () => {
+    return (
+        <div>
+            <h4>Login Please</h4>
+        </div>
+    );
+};
+
+export default Login;
--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,8 @@ import App from './App.jsx'
 import { createBrowserRouter, RouterProvider } from 'react-router'
 import Root from './assets/components/root/Root.jsx'
 import Home from './assets/components/home/Home.jsx'
+import Login from './assets/components/login/Login.jsx'
+import Resgister from './assets/components/register/Resgister.jsx'
 
 const router = createBrowserRouter([
   {
@@ -14,6 +16,14 @@ const router = createBrowserRouter([
       {
         index: true,
         Component: Home
+      },
+      {
+        path: '/login',
+        Component: Login
+      },
+      {
+        path: '/register',
+        Component: Resgister
       }
 
     ]
